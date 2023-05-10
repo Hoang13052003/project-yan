7f7447d4803c740f1fe4e57c0d899c16e3a20c56
Hoang13052003 <hoangvan13052003@gmail.com>
2023-05-11 03:30:06 +0700

    upload code

--- JS_Main/js_head.js
+++ JS_Main/js_head.js
@@ -1,26 +1,33 @@
 //js phần croll chuột lên xuống ẩn hiện thanh header
 var navbar = document.querySelector('.myheader');
-var slide = document.querySelector('.myslide');
 var prevScrollPos = window.pageYOffset;
 
 window.addEventListener('scroll', function() {
     var currentScrollPos = window.pageYOffset;
     var scrollPosition = window.scrollY;
     
-    if(scrollPosition > 130){
-        navbar.classList.remove('none-position-fixed');
-        slide.classList.remove('myslide-update-croll');
-        if (prevScrollPos > currentScrollPos) {
+    if(scrollPosition > 100){
+        if (prevScrollPos < currentScrollPos) {
             setTimeout(function(){
-                navbar.classList.remove('hidden-nav');
-            }, 200);
+                navbar.classList.remove('position-sticky');
+            }, 300);
         } else {
-            navbar.classList.add('hidden-nav');
+            setTimeout(function(){
+                navbar.classList.add('position-sticky');
+            }, 300);
         }
+        // slide.classList.remove('myslide-update-croll');
+        // if (prevScrollPos > currentScrollPos) {
+        //     setTimeout(function(){
+        //         navbar.classList.remove('hidden-nav');
+        //     }, 200);
+        // } else {
+        //     navbar.classList.add('hidden-nav');
+        // }
     }
     else{
-        navbar.classList.add('none-position-fixed');
-        slide.classList.add('myslide-update-croll');
+        navbar.classList.remove('position-sticky');
+        // slide.classList.add('myslide-update-croll');
     }
   prevScrollPos = currentScrollPos;
 });
